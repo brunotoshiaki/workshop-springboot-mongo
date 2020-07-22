04b60e29981cbe9e3c30489a41e0158f1f43df36
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2020-07-22 20:31:24 -0300

    Inserção de usuário com POST

--- src/main/java/br/com/toshiakibruno/workshopmongo/services/UserService.java
+++ src/main/java/br/com/toshiakibruno/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.toshiakibruno.workshopmongo.domain.User;
+import br.com.toshiakibruno.workshopmongo.dto.UserDTO;
 import br.com.toshiakibruno.workshopmongo.repository.UserRepository;
 import br.com.toshiakibruno.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -15,13 +16,21 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List <User>findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));	
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
