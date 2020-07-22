40804bc2c1ba3ad6fae7dfee42ac7cbdcd753949
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2020-07-22 20:10:54 -0300

    Obtendo um usuário por id

---
+++ src/main/java/br/com/toshiakibruno/workshopmongo/resources/exeption/StandardError.java
@@ -0,0 +1,25 @@
+package br.com.toshiakibruno.workshopmongo.resources.exeption;
+
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StandardError implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Long timeStamp;
+	private Integer status;
+	private String error;
+	private String message;
+	private String path;
+	
+
+}
---
+++ src/main/java/br/com/toshiakibruno/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,18 @@
+package br.com.toshiakibruno.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
